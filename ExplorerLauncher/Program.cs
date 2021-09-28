db40e77886a7d6329fa6291f06564adbae187230
oidvo <oidvo@DESKTOP-268AN4E>
2021-09-27 19:22:57 -0500

    Add project files.

---
+++ ExplorerLauncher/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oid.Applications.ExplorerLauncher
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{	
+				var startInfo = new ProcessStartInfo("C:\\Windows\\explorer.exe");
+				var realArgs = args?.Where(a => !string.IsNullOrWhiteSpace(a)).ToArray();
+				if (realArgs?.Any() ?? false)
+					startInfo.Arguments = string.Join(" ", realArgs);
+				startInfo.WindowStyle = ProcessWindowStyle.Normal;
+				Process.Start(startInfo);
+			}
+			catch { }
+		}
+	}
+}
